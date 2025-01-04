9c8e6d028d3891762cd4d5799e0a3979cda58b5a
Rodion <marpleway@Noutbuk-Rodion.local>
2025-01-04 23:30:48 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,48 +1,47 @@
 import readlineSync from 'readline-sync';
-import sayHello from './cli.js';
 
-const name = sayHello();
-
-const isEven = (num) => {
-  if (num % 2 === 0) {
-    return true;
-  }
-
-  return false;
-};
+const maxRounds = 3;
 
 const brainEven = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(gameDescription);
+
   // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < 3; i++) {
-    const rnd = Math.floor(Math.random() * 20);
-    console.log(`Question: ${rnd}`);
+  for (let i = 0; i < maxRounds; i++) {
+    const number = Math.floor(Math.random() * 100) + 1;
+    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+
+    console.log(`Question: ${number}`);
     const answer = readlineSync.question('Your answer: ');
-    if (answer === 'yes' && isEven(rnd)) {
+
+    if (answer === correctAnswer) {
       console.log('Correct!');
-    } else if (answer === 'no' && !isEven(rnd)) {
-      console.log('Correct!');
-    } else if (answer === 'yes' && !isEven(rnd)) {
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLets try again, ${name}!`);
-      return false;
-    } else if (answer === 'no' && isEven(rnd)) {
-      console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLets try again, ${name}!`);
-      return false;
-    } else { return false; }
+    } else {
+      console.log(`${answer} is the wrong answer ;(. The correct answer was ${correctAnswer}. Let's try again, ${name}!`);
+      return;
+    }
   }
   console.log(`Congratulations, ${name}!`);
-  return true;
 };
 
 const brainCalc = () => {
-  console.log('What is the result of the expression?');
+  const gameDescription = 'What is the result of the expression?';
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(gameDescription);
+
   // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < maxRounds; i++) {
     const operations = ['+', '-', '*'];
     const randomOperation = operations[Math.floor(Math.random() * operations.length)];
     const num1 = Math.floor(Math.random() * 25);
     const num2 = Math.floor(Math.random() * 25);
     let result = 0;
+
     switch (randomOperation) {
       case '+':
         result = num1 + num2;
@@ -54,19 +53,21 @@ const brainCalc = () => {
         result = num1 * num2;
         break;
       default:
-        return false;
+        console.log('Unknown operation!');
+        return;
     }
+
     console.log(`Question: ${num1} ${randomOperation} ${num2}`);
     const answer = readlineSync.question('Your answer: ');
+
     if (Number(answer) === result) {
       console.log('Correct!');
-    } else if (Number(answer) !== result) {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${result}. Let's try again, ${name}`);
-      return false;
+    } else {
+      console.log(`${answer} is the wrong answer ;(. The correct answer was ${result}. Let's try again, ${name}!`);
+      return;
     }
   }
   console.log(`Congratulations, ${name}!`);
-  return true;
 };
 
 export { brainCalc, brainEven };
