36b90c9abf67e814d71bd7209b1955fdb159ab21
Rodion <marpleway@Noutbuk-Rodion.local>
2025-01-05 18:57:55 +0300

    add brain-gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,22 @@
+import { getRandomNumber, findMax } from './utils.js';
+
+const gameDescription = 'Find the greatest common divisor of given numbers.';
+
+const generateRound = () => {
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
+  const maxNum = findMax(num1, num2);
+  const stack = [];
+  const question = `${num1} ${num2}`;
+
+  for (let i = 1; i <= maxNum; i += 1) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      stack.push(i);
+    }
+  }
+  const correctAnswer = String(stack.pop());
+
+  return [question, correctAnswer];
+};
+
+export { gameDescription, generateRound };
--- src/games/utils.js
+++ src/games/utils.js
@@ -1,3 +1,9 @@
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export default getRandomNumber;
+const findMax = (num1, num2) => {
+  if (num1 > num2) {
+    return num1;
+  }
+  return num2;
+};
+export { getRandomNumber, findMax };
